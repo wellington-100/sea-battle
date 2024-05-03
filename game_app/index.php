da21ff312358c8ba8083fe93b7680f0f4d0682c4
Wellington-100 <vasile.andruscenco@gmail.com>
2024-05-04 01:05:14 +0300

    organization by functions

--- game_app/index.php
+++ game_app/index.php
@@ -1,8 +1,22 @@
-<? require_once 'input.php'?>
+<? require_once 'lib.php'?>
+<? $map = load_map()?>
+<? $coords = get_coords($_GET)?>
+<? $map = shoot($map, $coords)?>
 
-<link rel="stylesheet" href="css/app.css">
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>SEA BATTLE</title>
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+        <? print (render_map($map, $y))?>
+        
+    </body>
+    </html>
+    
+<? save_map($map)?>
 
-<? require_once 'map.php'?>
-<? require_once 'template.php'?>
 
 
--- game_app/lib.php
+++ game_app/lib.php
@@ -1,6 +1,62 @@
 <?
+$y = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+function render_map($map, $y)
+{
+    $html = '<div class="yDimension">';
 
-function render_map (){
-    return "<div>THIS IS THE MAP</div>";
+    $html .= '<div class="map">';
+    for ($ri = 0; $ri < 10; $ri++) {
+        for ($ci = 0; $ci < 10; $ci++) {
+            $attributes = $map[$ri][$ci] == 1 ? 'class="ship sq"' : 'class="sq"';
+            $attributes .= " href=\"/?shoot={$ri}x{$ci}\" ";
+            $html .= '<div><a ' . $attributes . '></a></div>';
+        }
+    }
+    $html .= '</div>'; // Close map div
+
+    $html .= '<div class="y">';
+    for ($i = 0; $i < 10; $i++) {
+        $html .= '<div>' . $y[$i] . '</div>';
+    }
+    $html .= '</div>'; // Close y div
+
+    $html .= '</div>'; // Close yDimension div
+
+    $html .= '<div class="x">';
+    for ($x = 1; $x < 11; $x++) {
+        $html .= '<div>' . $x . '</div>';
+    }
+    $html .= '</div>'; // Close x div
+
+    return $html;
 }
+/////////////////////////////////////////
+
+function get_coords($request) {
+    if (isset($request['shoot'])) {
+        $coords = explode('x', $request['shoot']);
+        return $coords;
+    }
+    return false;
+}
+/////////////////////////////////////////
+
+function shoot($map, $coords) {
+    if ($coords) {
+        $map[$coords[0]][$coords[1]] = 1;
+    }
+    return $map;
+}
+/////////////////////////////////////////
+
+function save_map($map){
+    file_put_contents("./data/map.json", json_encode($map));
+}
+
+function load_map() {
+    return json_decode(file_get_contents("./data/map.json"), true) ;
+}
+
+
+
 ?>
